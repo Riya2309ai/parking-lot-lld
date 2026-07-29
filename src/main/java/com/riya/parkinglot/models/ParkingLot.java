a238232867554ca339799f450fad03c8385fad3c
riya2315 <85999450+riya2315@users.noreply.github.com>
2026-07-29 16:02:01 +0530

    Improved the design by adding service layer

--- src/main/java/com/riya/parkinglot/models/ParkingLot.java
+++ src/main/java/com/riya/parkinglot/models/ParkingLot.java
@@ -21,6 +21,7 @@ public class ParkingLot {
     private String address;
     private List<ParkingFloor> parkingFloors;
     private ParkingStrategy parkingStrategy;
+
     private Map<String, ParkingTicket> activeTickets;
     private FeeCalculator feeCalculator;
     private PaymentStrategy paymentStrategy;
@@ -34,47 +35,9 @@ public class ParkingLot {
         this.feeCalculator = feeCalculator;
         this.paymentStrategy = paymentStrategy;
     }
-    public ParkingTicket parkVehicle(Vehicle vehicle){
-        ParkingSpot parkingSpot = findAvailableSpot(vehicle);
 
-        if(parkingSpot == null){
-            throw new RuntimeException("Parking Lot is full");
-        }
-        parkingSpot.setOccupied(true);
 
-        String ticketId = UUID.randomUUID().toString();
-        ParkingTicket ticket = new ParkingTicket(ticketId, vehicle, parkingSpot);
 
-        activeTickets.put(ticketId, ticket);
-        return ticket;
 
-    }
 
-    public double unparkVehicle(ParkingTicket ticket){
-        ParkingSpot parkingSpot = ticket.getParkingSpot();
-        ticket.setExitTime(LocalDateTime.now());
-        double fee=feeCalculator.calculateFee(ticket);
-        boolean paymentSuccessful= paymentStrategy.pay(fee);
-        if(!paymentSuccessful){
-            throw new RuntimeException("Payment failed");
-        }
-        parkingSpot.setOccupied(false);
-        ticket.setTicketStatus(TicketStatus.PAID);
-        activeTickets.remove(ticket.getTicketId());
-
-        return fee;
-
-    }
-
-    private ParkingSpot findAvailableSpot(Vehicle vehicle) {
-        for(ParkingFloor floor : parkingFloors){
-
-            ParkingSpot spot = parkingStrategy.findSpot(floor.getParkingSpots());
-
-            if(spot != null && spot.canFitVehicle(vehicle)){
-                return spot;
-            }
-        }
-        return null;
-    }
 }
